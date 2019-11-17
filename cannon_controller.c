689ef99bfd8f7df03f041cb0b9bc8eec320ea952
benjam60 <erichseb@umich.edu>
2019-11-17 11:49:08 -0500

    better cannon controller

--- cannon_controller.c
+++ cannon_controller.c
@@ -2,25 +2,35 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-//add debugging code to driver -seems to be issue with fops
-//maybe no open or close method for fops!
-//THERES A NULL POINTER ERROR
 void main(void) {
 	int fd;
 	int ret;
-	printf("Starting");
-	fd = open("/dev/cannon", O_WRONLY | O_CREAT | O_TRUNC);
-	if (fd < 0) {
-		perror("Problem opening cannon");
-		exit(1);
+	char buffer[10];
+	int isACommand;	
+	while(1) {
+		printf("Type one of the following commands:\n");
+		printf("up | down | left | right | fire\n");
+		gets(buffer);
+		isACommand = (strstr(buffer, "up") != NULL) ||
+		       	     (strstr(buffer, "down") != NULL) ||
+			     (strstr(buffer, "left") != NULL) ||
+			     (strstr(buffer, "right") != NULL) ||
+			     (strstr(buffer, "fire") != NULL);
+		if (isACommand) {
+			fd = open("/dev/cannon", O_WRONLY | O_CREAT | O_TRUNC);
+			if (fd < 0) {
+				perror("Problem opening cannon");
+				exit(1);
+			}
+			write(fd, buffer, strlen(buffer) + 1);
+			write(fd, "stop", 5);
+			close(fd);
+		}
+		else { printf("Not a valid command"); }
+	
 	}
-	printf("Opened the cannon");
-	ret = write(fd, "up", 3);
-	printf("Writing to the cannon");
-	if (ret < 0) {
-		perror("Problem writing to cannon");
-	}
-//	close(fd);
 
 }
