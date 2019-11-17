9cff17da46ebd9674240c0b14d0eed2db09aaeec
benjam60 <erichseb@umich.edu>
2019-11-17 10:07:23 -0500

    cleaning up code

--- cannon_driver.c
+++ cannon_driver.c
@@ -28,36 +28,26 @@ struct cannon_info {
 static struct usb_driver cannon_driver;
 static struct usb_class_driver cannon_class;
 
-static void move_up(struct usb_device *dev, struct usb_interface *interface) {
-	unsigned int endpoint;
-	void * command;
-	int res;
-	unsigned char commandStr[8] = {0x02, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; 
-	int commandSize = 8;
-	printk(KERN_ALERT "CANNON WRITE STEP 1");
-	endpoint = usb_sndctrlpipe(dev, DEFAULT_CONTROL_ENDPOINT);
-	printk(KERN_ALERT "CANNON WRITE STEP 2");
-	command = kmalloc(commandSize, GFP_KERNEL);
-	printk(KERN_ALERT "CANNON WRITE STEP 3");
-	memcpy(command, commandStr, commandSize);
-	printk(KERN_ALERT "CANNON WRITE STEP 4");
-	res = usb_control_msg(dev, endpoint, CONTROL_REQUEST, CONTROL_REQUEST_TYPE, 0x00, 0x00, command, commandSize, 0);
-	printk(KERN_ALERT "CANNON WRITE STEP 5");
-	if (res < 0) {
-		printk(KERN_ALERT "Ben here: failed to send control message with error %d", res);
+static void move_up(struct usb_device *dev, struct usb_interface *interface);
+
+static int __init cannon_init(void) {
+	int register_result;
+	register_result = usb_register(&cannon_driver);
+	if (register_result < 0) {
+		printk(KERN_ALERT "Failed to register cannon driver. Erro number %d", register_result);
+		return register_result;
 	}
-	kfree(command);
-
-
+	printk(KERN_ALERT "Registered cannon driver");
+	return 0;
 }
 
 static int cannon_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
-	int retval;
+	int register_device_result;
 	struct cannon_info * cannon_device_info = NULL;
 	cannon_device_info = kzalloc(sizeof(struct cannon_info), GFP_KERNEL);
 	if (!cannon_device_info) {
-		printk(KERN_ALERT "device is out of memory");
+		printk(KERN_ALERT "Kernel is out of memory");
 		return 1;
 	}
 
@@ -65,14 +55,68 @@ static int cannon_probe(struct usb_interface *interface, const struct usb_device
 	cannon_device_info->interface = interface;
 	usb_set_intfdata(interface, cannon_device_info);
 	
-	retval = usb_register_dev(interface, &cannon_class);
-	if (retval) {
-		printk(KERN_ALERT "failed to register device with error code %d", retval);
-		return retval;
+	register_device_result = usb_register_dev(interface, &cannon_class);
+	if (register_device_result) {
+		printk(KERN_ALERT "Failed to register device with error code %d", register_device_result);
+		return register_device_result;
 	}
-	dev_info(&interface->dev, "Device attached to %d", interface->minor);
-	printk(KERN_ALERT "Ben here: My cannon has been plugged in");
+	dev_info(&interface->dev, "Cannon device attached to %d", interface->minor);
+	printk(KERN_ALERT "Cannon successfully connected to computer");
 	return 0;
+}
+
+static int cannon_open(struct inode *inode, struct file *file) { 
+	struct usb_interface *interface;
+	struct cannon_info *cannon_device_info = NULL;
+	int minorNum;
+	minorNum = iminor(inode);
+	interface = usb_find_interface(&cannon_driver, minorNum);
+	if (!interface) {
+		printk(KERN_ALERT "Could not find interface for minor number");
+		return 1;	
+	}
+	cannon_device_info = usb_get_intfdata(interface);
+	if (!cannon_device_info) {
+		printk(KERN_ALERT "Could not get cannon device info");
+		return 1;
+	}
+	file->private_data = cannon_device_info;
+	printk(KERN_ALERT "Opened cannon device");
+	return 0;
+}
+
+static ssize_t cannon_write(struct file *file, const char __user *user_buffer, size_t count, loff_t *ppos) {
+	struct cannon_info *cannon_device_info;
+	int max_input_size = 4;
+	char * buffer = NULL;
+	cannon_device_info = (struct cannon_info *) file->private_data;
+	buffer = kmalloc(max_input_size, GFP_KERNEL);
+	if (copy_from_user(buffer, user_buffer, max_input_size) < 0) {
+		printk(KERN_ALERT "Failed to copy data from user");
+	}
+	if (strcmp(buffer, "up") == 0) {
+		move_up(cannon_device_info->dev, cannon_device_info->interface);
+
+		/*unsigned int endpoint;
+		void * command;
+		int res;
+	unsigned char commandStr[8] = {0x02, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; 
+	int commandSize = 8;
+	endpoint = usb_sndctrlpipe(cannon_device_info->dev, DEFAULT_CONTROL_ENDPOINT);
+	command = kmalloc(commandSize, GFP_KERNEL);
+	memcpy(command, commandStr, commandSize);
+	res = usb_control_msg(cannon_device_info->dev, endpoint, CONTROL_REQUEST, CONTROL_REQUEST_TYPE, 0x00, 0x00, command, commandSize, 0);
+	printk(KERN_ALERT "CANNON WRITE STEP 5");
+	if (res < 0) {
+		printk(KERN_ALERT "Ben here: failed to send control message with error %d", res);
+	}
+	kfree(command);
+*/
+		//move_up(cannon_device_info->dev, cannon_device_info->interface);
+	}
+	kfree(buffer);
+	return 1;
+
 }
 
 static void cannon_disconnect(struct usb_interface *interface) {
@@ -86,76 +130,18 @@ static void cannon_disconnect(struct usb_interface *interface) {
 	printk(KERN_ALERT "Ben here: Removed cannon from the computer");
 }
 
-static struct usb_device_id cannon_table[] = {
-        { USB_DEVICE(CANNON_VENDOR_ID, CANNON_PRODUCT_ID) },
-        {}
-};
-
-MODULE_DEVICE_TABLE(usb, cannon_table);
-
-static int cannon_open(struct inode *inode, struct file *file) { 
-	struct usb_interface *interface;
-	struct cannon_info *cannon_device_info = NULL;
-	int minorNum;
-	minorNum = iminor(inode);
-	interface = usb_find_interface(&cannon_driver, minorNum);
-	if (!interface) {
-		printk(KERN_ALERT "could not find interface for minor number");
-		return 1;	
-	}
-	cannon_device_info = usb_get_intfdata(interface);
-	if (!cannon_device_info) {
-		printk(KERN_ALERT "could not get cannon device info");
-		return 1;
-	}
-	file->private_data = cannon_device_info;
-	printk(KERN_ALERT "cannon driver is opened");
-	return 0;
-}
-
-static ssize_t cannon_write(struct file *file, const char __user *user_buffer, size_t count, loff_t *ppos) {
-	struct cannon_info *cannon_device_info;
-	int max_input_size = 4;
-	char * buffer = NULL;
-	printk(KERN_ALERT "CANNON WRITE STEP -1");
-	cannon_device_info = (struct cannon_info *) file->private_data;
-	buffer = kmalloc(max_input_size, GFP_KERNEL);
-	printk(KERN_ALERT "CANNON WRITE STEP 0");
-	if (copy_from_user(buffer, user_buffer, max_input_size) < 0) {
-		printk(KERN_ALERT "failed to copy from user");
-	}
-	if (strcmp(buffer, "up") == 0) {
-		unsigned int endpoint;
-		void * command;
-		int res;
-	unsigned char commandStr[8] = {0x02, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; 
-	int commandSize = 8;
-	printk(KERN_ALERT "CANNON WRITE STEP 1");
-	endpoint = usb_sndctrlpipe(cannon_device_info->dev, DEFAULT_CONTROL_ENDPOINT);
-	printk(KERN_ALERT "CANNON WRITE STEP 2");
-	command = kmalloc(commandSize, GFP_KERNEL);
-	printk(KERN_ALERT "CANNON WRITE STEP 3");
-	memcpy(command, commandStr, commandSize);
-	printk(KERN_ALERT "CANNON WRITE STEP 4");
-	res = usb_control_msg(cannon_device_info->dev, endpoint, CONTROL_REQUEST, CONTROL_REQUEST_TYPE, 0x00, 0x00, command, commandSize, 0);
-	printk(KERN_ALERT "CANNON WRITE STEP 5");
-	if (res < 0) {
-		printk(KERN_ALERT "Ben here: failed to send control message with error %d", res);
-	}
-	kfree(command);
-
-		//move_up(cannon_device_info->dev, cannon_device_info->interface);
-	}
-	kfree(buffer);
-	return 1;
-
-}
 
 static struct file_operations cannon_fops = { 
 	.owner = THIS_MODULE,
 	.open  = cannon_open,
 	.write = cannon_write,
 };
+
+static struct usb_device_id cannon_table[] = {
+        { USB_DEVICE(CANNON_VENDOR_ID, CANNON_PRODUCT_ID) },
+        {}
+};
+
 
 static struct usb_driver cannon_driver =
 {
@@ -170,22 +156,31 @@ static struct usb_class_driver cannon_class = {
 	.minor_base = 5,
 };
 
-static int __init cannon_init(void) {
-        int result;
-	result = usb_register(&cannon_driver);
-	if (result < 0) {
-		printk(KERN_ALERT "usb_register failed. Erro number %d", result);
-		return -1;
-	}
-	printk(KERN_ALERT "Ben here: Registered cannon driver");
-        return 0;
-}
-
 static void __exit cannon_exit(void) {
 	printk(KERN_ALERT "Ben here: removed cannon driver");
 	usb_deregister(&cannon_driver);
  }
 
+
+MODULE_DEVICE_TABLE(usb, cannon_table);
+
 module_init(cannon_init);
 module_exit(cannon_exit);
+
+
+static void move_up(struct usb_device *dev, struct usb_interface *interface) {
+	unsigned int endpoint;
+	void * command;
+	int res;
+	unsigned char commandStr[8] = {0x02, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; 
+	int commandSize = 8;
+	endpoint = usb_sndctrlpipe(dev, DEFAULT_CONTROL_ENDPOINT);
+	command = kmalloc(commandSize, GFP_KERNEL);
+	memcpy(command, commandStr, commandSize);
+	res = usb_control_msg(dev, endpoint, CONTROL_REQUEST, CONTROL_REQUEST_TYPE, 0x00, 0x00, command, commandSize, 0);
+	if (res < 0) {
+		printk(KERN_ALERT "Ben here: failed to send control message with error %d", res);
+	}
+	kfree(command);
+}
 
